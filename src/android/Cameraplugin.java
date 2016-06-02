583d07d08c6c4b0d5e4e3ad16c95675f9c8a03b4
Gokul M.S <Gokulms@nl.neologix.in>
2016-06-02 19:23:31 +0530

    2nd commit

--- src/android/Cameraplugin.java
+++ src/android/Cameraplugin.java
@@ -8,9 +8,20 @@ import org.json.JSONException;
 
 
 
+import android.hardware.Camera;
+
+
+
+
+
+
 public class Cameraplugin extends CordovaPlugin {
+    Camera cameraObject = null;
+
     @Override
+
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
         if (action.equals("openCamera")) {
             String message = args.getString(0);
             this.openCamera(message, callbackContext);
@@ -20,6 +31,14 @@ public class Cameraplugin extends CordovaPlugin {
     }
 
     private void openCamera(String message, CallbackContext callbackContext) {
+        try {
+            cameraObject = Camera.open();
+        }
+
+        catch (RuntimeException e) {
+            System.err.println(e);
+            return;
+        }
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
