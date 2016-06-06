74cede52c382ecfa3d3d27a7ac4cfa2b9f187ba8
Gokul M.S <Gokulms@nl.neologix.in>
2016-06-06 16:20:33 +0530

    10th commit

--- www/cameraPlugin.js
+++ www/cameraPlugin.js
@@ -1,11 +1,12 @@
 var cameraplugin = {
-    openCamera: function(count,successCallback, errorCallback) {
+    openCamera: function(successCallback, errorCallback) {
+		count = 1;
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'Cameraplugin', // mapped to our native Java class called "MediaSizeCut"
             'openCamera', // with this action name
-            [1]
+            [count]
         ); 
     }
 };	
